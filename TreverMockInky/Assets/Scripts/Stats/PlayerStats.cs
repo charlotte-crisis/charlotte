c3b2aa08ece0b795310929f19419381127220a57
EGGYPLANT\zhanhong <choongorilla@gmail.com>
2023-06-06 16:16:07 +0800

    Add method to update stats
    Other scripts can call this public method to update the
    persistent stats.

--- TreverMockInky/Assets/Scripts/Stats/PlayerStats.cs
+++ TreverMockInky/Assets/Scripts/Stats/PlayerStats.cs
@@ -12,12 +12,42 @@ public class PlayerStats : MonoBehaviour
     // Static keyword is important.
     public static PlayerStats Instance;
 
+    // Stats to be decided at the start of game.
+    // Can refactor name next time if needed.
+    public int stat_intelligence;
+    public int stat_charisma;
+    public int stat_confidence;
+    public int stat_empathy;
+
     private void Awake()
     {
+        // Start of new code (avoid duplicate GameObject)
+        if (Instance != null) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         // Singleton Instance.
         Instance = this;
 
         // This will ensure this gameobject doesn't get destroyed over scenes.
         DontDestroyOnLoad(gameObject);
+    }
+
+    // Update the stats through this method.
+    public void UpdatePlayerStats(
+            int intelligence,
+            int charisma,
+            int confidence,
+            int empathy)
+    {
+        Instance.stat_intelligence = intelligence;
+        Instance.stat_charisma = charisma;
+        Instance.stat_confidence = confidence;
+        Instance.stat_empathy = empathy;
+
+        Debug.Log("Instance Updated: ");
+        Debug.Log("Int: " + Instance.stat_intelligence);
     }
 }
